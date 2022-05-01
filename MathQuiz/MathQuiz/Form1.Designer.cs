233e47a29a5a68809e19952ca563b64d8135da3a
Amanda Stokes <agandstuff@outlook.com>
2022-04-30 22:58:51 -0600

    4-30-22 assignment update

--- MathQuiz/MathQuiz/Form1.Designer.cs
+++ MathQuiz/MathQuiz/Form1.Designer.cs
@@ -65,20 +65,20 @@
             // 
             this.timeLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.timeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.timeLabel.Location = new System.Drawing.Point(836, 24);
+            this.timeLabel.Location = new System.Drawing.Point(711, 25);
             this.timeLabel.Name = "timeLabel";
-            this.timeLabel.Size = new System.Drawing.Size(275, 61);
+            this.timeLabel.Size = new System.Drawing.Size(426, 61);
             this.timeLabel.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(566, 25);
+            this.label1.Location = new System.Drawing.Point(425, 25);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(247, 61);
+            this.label1.Size = new System.Drawing.Size(262, 61);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Time Left";
+            this.label1.Text = "Time Left:";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // plusLeftLabel
